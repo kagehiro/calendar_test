4c4be5683ea9c893a5a6382b4a4d3916a064fabe
kagehiro <rent.angel.fudoku@gmail.com>
2018-10-31 17:21:28 +0900

    commit 1

--- js/calender.js
+++ js/calender.js
@@ -1,52 +1,16 @@
-//トップページスライダー
-$(function() {
-  $('.not-enough-slider').slick({
-    infinite: true,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    arrows: false
-  });
-});
 
-// 一定量スクロールでメニュー固定
-$(function(){
-  var fix    = $(".global-menu");
-  var fixTop = fix.offset().top;
-  $(window).scroll(function () {
-    if($(window).scrollTop() >= fixTop) {
-      fix.css("position","fixed");
-      fix.css("top","0");
-    } else {
-      fix.css("position","");
-      fix.css("top","");
-    }
-  });
-});
+//現在日時表示
+function toDateShow() {
+  let toDate = new Date();
+  let year = toDate.getFullYear();
+  let month = toDate.getMonth() + 1;
+  let date = toDate.getDate();
+  let day = toDate.getDay();
 
-// 一定量スクロールでメニューに「おてつだいネットワークス」表示
-$(function(){
-  var fix    = $(".title-sp");
-  var fixTop = fix.offset().top;
-  $(window).scroll(function () {
-    if($(window).scrollTop() >= fixTop) {
-      fix.css("display","inline-block");
-    } else {
-      fix.css("display","none");
-    }
-  });
-});
+  let htmlShow = year + '/' + month + '/' + date + '(' + day + ')';
 
-//HEADER アコーディオン
-$(document).ready(function(){
-	$(".global-menu-icon").click(function(){
-		$(this).next().slideToggle(300);
-	});
-});
+  let target_p = document.getElementById("todate");
+  target_p.innerHTML = htmlShow;
 
-//FOOTER アコーディオン
-$(document).ready(function(){
-	$(".a_item").click(function(){
-		$(this).next().slideToggle(300);
-	});
-});
+}
+window.onload = toDateShow;
--- templates/calender.php
+++ templates/calender.php
@@ -1,15 +1,32 @@
-<section id="registration">
-  <div class="inbox">
-    <h2>新しい人材の見つけ方が、<br />今はじまっています</h2>
-    <div class="button">
-      <a href="">資料を請求する</a>
-    </div>
-    <div class="button orange">
-      <a href="">新規登録する</a>
-    </div>
-    <p>
-      お問い合わせ<br />
-      <a href="tel:0452172766"><img src="images/icons/phone.png" />03-5217-2766</a>　<a href="#"><img src="images/icons/mail.png" />こちらをクリック</a>
-    </p>
-  </div>
-</section>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>カレンダー＊テスト</title>
+  <meta name="description" content="試作したカレンダー">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+  <p>テストカレンダー</p>
+  <p id="todate"></p>
+
+  <table class="calender_table">
+    <tr>
+      <th>日</th>
+      <th>月</th>
+      <th>火</th>
+      <th>水</th>
+      <th>木</th>
+      <th>金</th>
+      <th>土</th>
+    </tr>
+  </table>
+
+
+
+  <script type="text/javascript" src="../js/calender.js"></script>
+</body>
+</html>
